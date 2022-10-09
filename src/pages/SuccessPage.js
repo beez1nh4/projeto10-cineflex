74fdbb29bf2db44934d00541886e16ce7ba17305
Melissa <melmihomel@gmail.com>
2022-10-09 15:53:59 -0300

    versao com formatacao do cpf

--- src/pages/SuccessPage.js
+++ src/pages/SuccessPage.js
@@ -4,6 +4,15 @@ export default function SuccessPage({request, setRequest}) {
     const nameFormat = request.name.split(/ /g).map(word =>
         `${word.substring(0,1).toUpperCase()}${word.substring(1)}`)
     .join(" "); //coloca a primeira letra em maiúsculo
+    let cpfFormat = ""
+    for (let i=0; i< request.cpf.length; i++){
+        cpfFormat+= request.cpf[i]
+        if(i === 2 || i === 5){
+            cpfFormat+= "."
+        } if (i===8){
+            cpfFormat+="-"
+        }
+    }
     return (
         <>
         <SuccessContainer>
@@ -12,10 +21,13 @@ export default function SuccessPage({request, setRequest}) {
         <Information>{request.title}</Information>
         <Information>{request.date} {request.time}</Information>
         <Title>Ingressos</Title>
-        {request.ids.map((num) => <Information>Assento {num}</Information>)}
+        {request.ids.map((num) => <Information key={num}>Assento {num}</Information>)}
         <Title>Comprador</Title>
         <Information>Nome: {nameFormat}</Information>
-        <Information>CPF: {request.cpf}</Information>
+        <Information>CPF: {cpfFormat}</Information>
+        <AlignButton>
+        <HomeButton>Voltar para Home</HomeButton>
+        </AlignButton>
         </SuccessContainer>
         </>
     )
@@ -35,7 +47,6 @@ const Sucesso = styled.div`
     color: #247A6B;
     margin-bottom: 67px;
 `
-
 const Title = styled.div`
     font-family: 'Roboto';
     font-style: normal;
@@ -47,7 +58,6 @@ const Title = styled.div`
     margin-bottom: 10px;
     margin-left: 28px;
 `
-
 const Information = styled.div`
     font-family: 'Roboto';
     font-style: normal;
@@ -58,4 +68,24 @@ const Information = styled.div`
     color: #293845;
     margin-left: 28px;
     margin-bottom: 10px;
+`
+const HomeButton = styled.button`
+    width: 225px;
+    height: 42px;
+    background: #E8833A;
+    border-radius: 3px;
+    font-family: 'Roboto';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 21px;
+    text-align: center;
+    letter-spacing: 0.04em;
+    color: #FFFFFF;
+    align-self: center;
+`
+const AlignButton = styled.div`
+    margin-top: 96px;
+    display: flex;
+    justify-content: center;
 `
